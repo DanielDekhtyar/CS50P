357b33ddf785f31b49a5aad9399ee56c194ec292
Daniel Dekhtyar <denik2707@gmail.com>
2023-11-09 11:13:55 +0200

    initial version

---
+++ Problem Set 7 - Regular Expressions/numb3rs.py
@@ -0,0 +1,21 @@
+# Learning Python with CS50
+# NUMB3RS
+# https://cs50.harvard.edu/python/2022/psets/7/numb3rs/
+
+
+import re
+
+
+
+def main():
+    print(validate(input("IPv4 Address: ")))
+
+
+def validate(ip):
+    if re.search(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{,3}$", ip):
+        return True
+    else:
+        return False
+
+if __name__ == "__main__":
+    main()
