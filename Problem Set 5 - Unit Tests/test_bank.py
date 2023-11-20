1e7c59fafc6ecd85af7ff40b3fe91b237f208ff8
Daniel Dekhtyar <denik2707@gmail.com>
2023-11-20 23:30:45 +0200

    Problem Set 5 - Unit Tests is done and submitted successfully

--- Problem Set 5 - Unit Tests/test_bank.py
+++ Problem Set 5 - Unit Tests/test_bank.py
@@ -6,27 +6,27 @@ from bank import value
 
 
 def test_0():
-  assert value("Hello") == 0
-  assert value("H") != 0
-  assert value("Hello World") == 0
-  assert value("Hello World!") == 0
-  assert value("Hi") != 0
+    assert value("Hello") == 0
+    assert value("H") != 0
+    assert value("Hello World") == 0
+    assert value("Hello World!") == 0
+    assert value("Hi") != 0
 
 
 def test_20():
-  assert value("Hi") == 20
-  assert value("Hello") != 20
-  assert value("Hello World") != 20
-  assert value("Howdy !") == 20
-  assert value("Good bye") != 20
+    assert value("Hi") == 20
+    assert value("Hello") != 20
+    assert value("Hello World") != 20
+    assert value("Howdy !") == 20
+    assert value("Good bye") != 20
 
 
 def test_100():
-  assert value("Hello World") != 100
-  assert value("Hello World!") != 100
-  assert value("Howdy !") != 100
-  assert value("Good bye") == 100
-  assert value("Goodbye") == 100
-  assert value("!@#$%^&*()") == 100
-  assert value("1234567890") == 100
-  assert value("1 2 3 4 5 6 7 8 9 0") == 100
+    assert value("Hello World") != 100
+    assert value("Hello World!") != 100
+    assert value("Howdy !") != 100
+    assert value("Good bye") == 100
+    assert value("Goodbye") == 100
+    assert value("!@#$%^&*()") == 100
+    assert value("1234567890") == 100
+    assert value("1 2 3 4 5 6 7 8 9 0") == 100
--- Problem Set 5 - Unit Tests/test_twttr.py
+++ Problem Set 5 - Unit Tests/test_twttr.py
@@ -4,35 +4,37 @@
 
 from twttr import shorten
 
+
 def main():
-  test_text()
-  test_num()
-  test_special_char()
+    test_text()
+    test_num()
+    test_special_char()
 
 
 def test_text():
-  assert shorten("twitter") == "twttr"
-  assert shorten("Twitter") == "Twttr"
-  assert shorten("HELLO") == "HLL"
-  assert shorten("Twitter is awesome") == "Twttr s wsm"
-  assert shorten("Hello World") == "Hll Wrld"
-  assert shorten("Hello World!") == "Hll Wrld!"
+    assert shorten("twitter") == "twttr"
+    assert shorten("Twitter") == "Twttr"
+    assert shorten("HELLO") == "HLL"
+    assert shorten("Twitter is awesome") == "Twttr s wsm"
+    assert shorten("Hello World") == "Hll Wrld"
+    assert shorten("Hello World!") == "Hll Wrld!"
 
 
 def test_num():
-  assert shorten("123") == "123"
-  assert shorten("123 456") == "123 456"
-  assert shorten("-1") == "-1"
-  assert shorten("-1 -2") == "-1 -2"
-  assert shorten("0") == "0"
-  assert shorten("1.852") == "1.852"
-  assert shorten("1.852.852") == "1.852.852"
-  assert shorten("-5.612") == "-5.612"
+    assert shorten("123") == "123"
+    assert shorten("123 456") == "123 456"
+    assert shorten("-1") == "-1"
+    assert shorten("-1 -2") == "-1 -2"
+    assert shorten("0") == "0"
+    assert shorten("1.852") == "1.852"
+    assert shorten("1.852.852") == "1.852.852"
+    assert shorten("-5.612") == "-5.612"
 
 
 def test_special_char():
-  assert shorten("!@#$%^&*()_+-={}[]:;'<>?,./") == "!@#$%^&*()_+-={}[]:;'<>?,./"
-  assert shorten("`~@#$%^&*()_+-={}[]:;'<>?,./") == "`~@#$%^&*()_+-={}[]:;'<>?,./"
-  
+    assert shorten("!@#$%^&*()_+-={}[]:;'<>?,./") == "!@#$%^&*()_+-={}[]:;'<>?,./"
+    assert shorten("`~@#$%^&*()_+-={}[]:;'<>?,./") == "`~@#$%^&*()_+-={}[]:;'<>?,./"
+
+
 if __name__ == "__main__":
-  main()
+    main()
