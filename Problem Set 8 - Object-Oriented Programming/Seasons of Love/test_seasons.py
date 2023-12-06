e3b1cf368d72bf9c7490ccdaf87c919f5bb81d27
Daniel Dekhtyar <denik2707@gmail.com>
2023-12-06 13:53:10 +0200

    Problem Set 8 - Object-Oriented Programming is done and submitted successfully

--- Problem Set 8 - Object-Oriented Programming/Seasons of Love/test_seasons.py
+++ Problem Set 8 - Object-Oriented Programming/Seasons of Love/test_seasons.py
@@ -7,9 +7,23 @@ from seasons import numbers_to_words
 
 
 def test_date_to_minutes():
-    assert numbers_to_words(10694880) == "Ten million, six hundred ninety-four thousand, eight hundred eighty"
-    assert numbers_to_words(13096800) == "Thirteen million, ninety-six thousand, eight hundred"
-    assert numbers_to_words(1833120) == "One million, eight hundred thirty-three thousand, one hundred twenty"
-    assert numbers_to_words(13377600) == "Thirteen million, three hundred seventy-seven thousand, six hundred"
-    assert numbers_to_words(12044160) == "Twelve million, forty-four thousand, one hundred sixty"
-    
+    assert (
+        numbers_to_words(10694880)
+        == "Ten million, six hundred ninety-four thousand, eight hundred eighty"
+    )
+    assert (
+        numbers_to_words(13096800)
+        == "Thirteen million, ninety-six thousand, eight hundred"
+    )
+    assert (
+        numbers_to_words(1833120)
+        == "One million, eight hundred thirty-three thousand, one hundred twenty"
+    )
+    assert (
+        numbers_to_words(13377600)
+        == "Thirteen million, three hundred seventy-seven thousand, six hundred"
+    )
+    assert (
+        numbers_to_words(12044160)
+        == "Twelve million, forty-four thousand, one hundred sixty"
+    )
